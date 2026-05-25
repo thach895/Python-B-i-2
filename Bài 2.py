bd3758adfa7b72519e247b992e021fe627ae83fc
thach895 <ngo67775@gmail.com>
2026-05-25 16:20:34 +0700

    Add files via upload

---
+++ Bài 2.py
@@ -0,0 +1,11 @@
+print(" --- HỆ THỐNG NHẬP CHỈ SỐ SINH TỒN --- ")
+
+name_patient = input("Nhập tên bệnh nhân : ")
+weight = float(input("Nhập cân nặng bệnh nhân : "))
+
+print(" --- KIỂM TRA DỮ LIỆU LƯU TRỮ --- ")
+print("Bệnh nhân :", name_patient)
+print("Cân nặng đã nhập :", weight)
+
+# Kiểm tra kiểu dữ liệu
+print("CẢNH BÁO - Kiểu dữ liệu đang lưu là :", type(weight))
