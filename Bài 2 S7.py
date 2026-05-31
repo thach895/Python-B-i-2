a6625281dc6358ea367c504bc515f54fe83b8df4
thach895 <ngo67775@gmail.com>
2026-05-31 19:55:52 +0700

    Add files via upload

---
+++ Bài 2 S7.py
@@ -0,0 +1,16 @@
+student_name = "  nguYEn vAn a  "
+student_code = "  rk-001-python  "
+email = "  Student01@GMAIL.COM  "
+
+student_name=student_name.strip()
+student_name=student_name.title()
+
+student_code=student_code.strip()
+student_code=student_code.upper()
+
+email=email.strip()
+email=email.lower()
+
+print("Họ tên:", student_name)
+print("Mã học viên:", student_code)
+print("Email:", email)
